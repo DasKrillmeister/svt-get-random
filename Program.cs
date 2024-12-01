b969dfa92712b8d713eecd5b33736fddf4609163
DasKrillmeister <krillmeister@gmail.com>
2024-12-01 22:22:32 +0100

    add flashback endpoint

--- Program.cs
+++ Program.cs
@@ -16,4 +16,16 @@ app.MapPost("/news", async context =>
     await context.Response.WriteAsync(randomItem?.Summary?.Text ?? "Eror :(");
 });
 
+var flashbackRssUrl = "https://www.flashback.org/external.php?type=rss2&forumids=165,68,181,26,21,338,150,226,337,34,77,147,508,535";
+app.MapPost("/flashback", async context =>
+{
+    context.Request.Body.Dispose();
+    using var rssReader = XmlReader.Create(flashbackRssUrl);
+    var feed = SyndicationFeed.Load(rssReader);
+    var items = feed.Items.ToArray();
+    var randomIndex = new Random().Next(items.Length);
+    var randomItem = items[randomIndex];
+    await context.Response.WriteAsync(randomItem?.Title?.Text ?? "Eror :(");
+});
+
 app.Run();
